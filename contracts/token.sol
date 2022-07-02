56605b6447b77122711a14085d3fcba4d0cde01f
imroshin <imroshin@gmail.com>
2022-07-02 17:13:51 +0530

    token

---
+++ contracts/token.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+// import "../interfaces/ICoin.sol";
+
+contract StableCoin is ERC20, Ownable {
+    constructor() ERC20("Stable Coin","STBLE") {
+    }
+
+    function mint(address account, uint256 amount) onlyOwner external  returns(bool){
+        _mint(account, amount);
+        return true;
+    }
+
+    function burn(address account, uint256 amount) onlyOwner external returns(bool){
+        _burn(account, amount);
+        return true;
+    }
+    
+}
