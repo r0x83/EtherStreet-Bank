3ccff4b51673c898eba2518be9b1cfa0194ed603
imroshin <imroshin@gmail.com>
2022-07-03 12:20:58 +0530

    added components

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,18 @@
 import './App.css';
 import {ethers} from 'ethers';
-import {useEffect, useState} from 'react';
-
+import {useState} from 'react';
 import Vault from './EtherVault.json';
-import Token from './StableCoin.json';
+import Balances from '.components/Balances.js';
+import Borrow from '.components/Borrow.js';
+import Repay from '.components/Repay.js';
+
 
 const vaultAddress = "0x0b82fA709CC4c4e026e4F05eE4D13Fa9C11cfd33"; //address of the deployed vault
 
 function App() {
-
-  const [provider, setProvider] = useState(undefined);
+  // const [provider, setProvider] = useState(undefined);
   const [accounts, setAccounts] = useState([]);
-  const [balance, setBalance] = useState(0);
-
   const [vaultContract, setVaultContract] = useState(undefined);
-  const [tokenContract, setTokenContract] = useState(undefined);
 
 
   const isConnected = () => Boolean(accounts[0]);
@@ -33,11 +31,12 @@ function App() {
     if (window.ethereum) {
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const signer = provider.getSigner(); //to sign the transaction
-      const contract = new ethers.Contract(
+      const vaultContract = new ethers.Contract(
         vaultAddress,
         Vault.abi,
         signer
       );
+      setVaultContract(vaultContract);
     }  
   }
 
@@ -49,6 +48,11 @@ function App() {
             <p>
               Welcome {accounts[0]?.substring(0,10)}...
             </p>
+            <div className='components'>
+              <Balances/>
+              <Borrow/>
+              <Repay/>
+            </div>
           </div>
         ) : (
           <div>
